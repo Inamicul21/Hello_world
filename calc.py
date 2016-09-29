ed0c8788728b1c8ae7982b98f3c148159992fed0
Inamicul21 <fbogdan21@gmail.com>
2016-09-30 01:08:35 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,32 +1,78 @@
+# Florea Bogdan 30.09.2016
+
+#importare librarii
 import sys
+from random import randint
 import os
 
+# definire functii
+def add(x, y):
+   
+   return x + y
 
-#a little calculator
+def subtract(x, y):
+   
+   return x - y
 
-#don't be mad if you don't like it
+def multiply(x, y):
+   pass
 
-while True:
-   print("Options:")
-   print("Enter 'add' to add two numbers")
-   print("Enter 'subtract' to subtract two numbers")
-   print("Enter 'multiply' to multiply two numbers")
-   print("Enter 'divide' to divide two numbers")
-   print("Enter 'quit' to end the program")
-   user_input = input(": ")
+   return x * y
 
-   if user_input == "quit":
-      break
-   elif user_input == "add":
-      num1 = float(input("Enter a number: "))
- 	  num2 = float(input("Enter another number: "))
- 	  result = str(num1 + num2)
-  	print("The answer is " + result)
-   elif user_input == "subtract":
-      num1 =float(input("Introdu un numar: "))
-   elif user_input == "multiply":
-      ...
-   elif user_input == "divide":
-      ...
-   else:
-      print("Unknown input")
+def divide(x, y):
+
+   return x / y
+
+def my_friend():
+   print ("Hello my friend\n")
+
+def input_fNum():
+     val =  int(input("Introdu primul numar: "))
+     return val
+
+def input_sNum():
+      val = int(input("Introdu al doilea numar: "))
+      return val
+      
+      # random number on demand
+def talent():
+   print("Numarul tau random este:",randint(0,9))
+
+# take input from the user
+print("Select operation.")
+print("1.Aduna")
+print("2.Scadere")
+print("3.Multiplicare")
+print("4.Impartire")
+print("5.Hello friend!")
+print("6.Talentul se dezvaluie")
+
+choice = input("Enter alegere(1/2/3/4/5):")
+
+if choice == '1':
+   num1 = input_fNum() 
+   num2 = input_sNum()
+   print(num1,"+",num2,"=", add(num1,num2), "\nFacem adunari vai\n")
+
+elif choice == '2':
+   num1 = input_fNum() 
+   num2 = input_sNum()
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   num1 = input_fNum() 
+   num2 = input_sNum()
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   num1 = input_fNum() 
+   num2 = input_sNum()
+   print(num1,"/",num2,"=", divide(num1,num2))
+elif choice == '5':
+         my_friend()
+elif choice == '6':
+         talent()
+elif choice != ('1','2','3','4','5','6'):
+         print("v3rs1on1.0.py\n")
+else:
+   print("Input invalid! Mai incearca..\m")
