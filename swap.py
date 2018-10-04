077f99cb78ea30a4b03822563fd9d34557ef0e70
Bgd <fbogdan21@gmail.com>
2018-10-04 10:21:35 +0300

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,5 @@
+a = input("Introdu prima variabila: ")
+b = input("Introdu a doua variabila: ")
+print("Variabile tale sunt:",format(a),"si",format(b))
+b, a = a, b
+print("Variabilele tale inversate sunt:",format(a),"si",format(b))
