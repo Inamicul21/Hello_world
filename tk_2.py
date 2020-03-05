1e9576d708dfe87422cf30d07b47d9c19e56e3d9
Bgd <fbogdan21@gmail.com>
2020-03-05 18:52:40 +0200

    Add files via upload

---
+++ tk_2.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+# initiere applicatie Tkinter
+root = Tk()
+
+m1 = PanedWindow(root)
+m1.pack(fill=BOTH, expand=1)
+
+left = Entry(m1, bd = 5)
+m1.add(left)
+
+m2 = PanedWindow(m1, orient = VERTICAL)
+m1.add(m2)
+
+top = Scale(m2, orient = HORIZONTAL)
+m2.add(top)
+
+btn = Button(m2, text='ok')
+m2.add(btn)
+
+root.mainloop()
